a5596f4241067b66b321dfd1978c4a5dd23d0a4c
Andrew Fox <90468039+bobthecorn2000@users.noreply.github.com>
2023-09-24 07:41:29 -0600

    Restructured all files to be prettier

--- demo/demo.py
+++ demo/demo.py
@@ -1,17 +1,1 @@
-import pickle
-def main():
-    courses = [["Python", 3],
-               ["Trig", 3],
-               ["Physics", 4],
-               ["Yoga", 2]]
-    with open("classes.bin", "wb") as file:
-        pickle.dump(courses, file)
-    with open("classes.bin", "rb") as file:
-        course_list = pickle.load(file)
-    i = 0
-    while i < len(course_list):
-        course = course_list[i]
-        print(course[0], course[1], end=" ")
-        i += 2
-
-main()
+Print(n oops lol I was testing a python program)
